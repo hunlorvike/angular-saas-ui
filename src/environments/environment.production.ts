529cf783a54f41ac9559ec19abff1832c227d591
Nguyễn Việt Hưng <hungcutedethuongg@gmail.com>
2024-09-01 02:14:27 +0700

    feat: add eslint, prettier and config profile: dev, prod

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    envName: 'PRODUCTION',
+    production: true,
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    envName: 'DEVELOPMENT',
+    production: false,
+};
