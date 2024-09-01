556f13a54250418e444459012e927bff615241dd
Nguyễn Việt Hưng <hungcutedethuongg@gmail.com>
2024-09-02 03:16:48 +0700

    add: ngxs - state management

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,27 @@ import {
     provideClientHydration,
     withEventReplay,
 } from '@angular/platform-browser';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
+import { withNgxsFormPlugin } from '@ngxs/form-plugin';
+import { withNgxsLoggerPlugin } from '@ngxs/logger-plugin';
+import { withNgxsRouterPlugin } from '@ngxs/router-plugin';
+import { withNgxsStoragePlugin } from '@ngxs/storage-plugin';
+import { provideStore } from '@ngxs/store';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
         provideClientHydration(withEventReplay()),
+        provideStore(
+            [],
+            withNgxsReduxDevtoolsPlugin(),
+            withNgxsFormPlugin(),
+            withNgxsLoggerPlugin(),
+            withNgxsRouterPlugin(),
+            withNgxsStoragePlugin({
+                keys: ['auth'],
+            })
+        ),
     ],
 };
